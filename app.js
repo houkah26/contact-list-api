d2ed2228387e97c5beae62819395183b72eae384
houkah26 <houkah26@gmail.com>
2017-11-08 13:17:48 -0700

    add CORS headers

--- app.js
+++ app.js
@@ -8,9 +8,21 @@ const app = express();
 // Log requests to the console
 app.use(logger("dev"));
 
+// Enable CORS from client-side
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Require our routes into the application
 require("./server/routes")(app);
